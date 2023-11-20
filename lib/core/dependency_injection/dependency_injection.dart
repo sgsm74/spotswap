3f27845fea5a0b564fe4fd866555d6e887460f28
sgsm74 <saeed.razii93@gmail.com>
2023-11-20 14:37:03 +0330

    fix: some missed import

--- lib/core/dependency_injection/dependency_injection.dart
+++ lib/core/dependency_injection/dependency_injection.dart
@@ -1,10 +1,14 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:spotswap/core/consts/consts.dart';
+import 'package:spotswap/core/services/connection_checker.dart';
 import 'package:spotswap/core/services/http_service.dart';
 import 'package:spotswap/data/datasource/network_datasource.dart';
 import 'package:spotswap/data/repository/repository.dart';
 import 'package:spotswap/domain/repository/repository.dart';
+import 'package:spotswap/domain/usecases/authentication_usecase.dart';
+import 'package:spotswap/presentation/provider/spot_swap_model.dart';
 
 final sl = GetIt.instance;
 Future<void> init() async {
@@ -16,6 +20,16 @@ Future<void> init() async {
           baseUrl: ServerPaths.baseUrl,
         ),
       ),
+    ),
+  );
+  //network
+  sl.registerLazySingleton<NetworkInfo>(
+    () => NetworkInfoImpl(dataConnectionChecker: sl()),
+  );
+  //connection checker
+  sl.registerLazySingleton(
+    () => InternetConnectionChecker.createInstance(
+      checkTimeout: const Duration(milliseconds: 90),
     ),
   );
   //datasource
@@ -30,5 +44,13 @@ Future<void> init() async {
       networkDatasource: sl(),
       networkInfo: sl(),
     ),
+  );
+  //usecase
+  sl.registerLazySingleton(
+    () => AuthenticationUseCase(repository: sl()),
+  );
+  //provider
+  sl.registerLazySingleton(
+    () => SpotSwapModel(authenticationUseCase: sl()),
   );
 }
